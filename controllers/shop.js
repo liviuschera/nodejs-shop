32a7539c1586ff34297c73fab77c514173d93e28
Liviu Schera <liviu115@gmail.com>
2019-05-22 16:18:11 +0930

    first commit

---
+++ controllers/shop.js
@@ -0,0 +1,44 @@
+const Product = require('../models/product');
+
+exports.getProducts = async (req, res, next) => {
+  res.render('shop/product-list', {
+    prods: await Product.fetchAll(),
+    pageTitle: 'All Products',
+    path: '/products'
+  });
+};
+
+exports.getProduct = async (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log(await Product.findById(prodId));
+  res.redirect("/")
+};
+
+exports.getIndex = async (req, res, next) => {
+  res.render('shop/index', {
+    prods: await Product.fetchAll(),
+    pageTitle: 'Shop',
+    path: '/'
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
---
+++ models/product.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+const p = path.resolve('data/products.json');
+
+async function getProductsFromFile(id = null) {
+  try {
+    const products = JSON.parse(await readFileAsync(p));
+    if (id) {
+      return products.find(product => product.id === id);
+    }
+    return products;
+  } catch (err) {
+    return [];
+  }
+}
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+
+  async save() {
+    try {
+      this.id = Math.random().toString();
+      const products = await getProductsFromFile();
+      products.push(this);
+
+      writeFileAsync(p, JSON.stringify(products))
+    } catch (err) {
+      console.error("Save file error: ", err);
+
+    }
+  }
+
+  static async fetchAll() {
+    return getProductsFromFile();
+  }
+
+  static async findById(id) {
+    return getProductsFromFile(id);
+  }
+};
